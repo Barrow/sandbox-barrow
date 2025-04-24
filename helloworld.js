62ff2bc58538de6fa7b949cfcb51549d109433a7
Barrow <barrow.dev@gmail.com>
2025-04-24 16:04:54 -0400

    Console logging on init

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,4 @@
-Hooks.on('init', () => {
-    console.log('Barrow\'s Sandbox: SANDBOX.helloworld')
-})
+export let log = (...args) => console.log("%cBarrow's Sandbox | ", "color:GreenYellow", ...args);
+Hooks.on("init", () => {
+	log("Initialized. Hello world!");
+});
