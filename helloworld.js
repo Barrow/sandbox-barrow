a21b2c37504fdfbad3041f115a63d9a1e007979f
Barrow <barrow.dev@gmail.com>
2025-04-25 17:08:07 -0400

    Debug/warning

--- helloworld.js
+++ helloworld.js
@@ -1,10 +1,49 @@
-// Inspired by/stolen from IronMonk's Always HP, thank you!
+// Inspired by/blatantly stolen from IronMonk's Always HP, thank you!
 import { registerSettings } from "./settings.js";
-export let log = (...args) =>
-	console.log("%cBarrow's Sandbox | ", "color:GreenYellow", ...args);
+
+const CONSTANTS = {
+	DEBUG: false,
+};
+
+export let log = (...args) => {
+	console.log("%cBarrow's Sandbox", "color:teal", " | ", ...args);
+};
+
+export let warn = (...args) => {
+	if (CONSTANTS.DEBUG) {
+		console.log(
+			"%cBarrow's Sandbox",
+			"color:teal",
+			" | ",
+			"%cWARN",
+			"color:yellow",
+			" | ",
+			...args
+		);
+	}
+};
+
+export let debug = (...args) => {
+	if (CONSTANTS.DEBUG) {
+		console.log(
+			"%cBarrow's Sandbox",
+			"color:teal",
+			" | ",
+			"%cWARN",
+			"color:yellow",
+			" | ",
+			...args
+		);
+	}
+};
+
+export let setting = (key) => {
+	return game.settings.get("sandbox-barrow", key);
+};
 
 Hooks.on("init", () => {
 	registerSettings();
 
 	log("Initialized. Hello world!");
+	debug("This is a debugging message.");
 });
